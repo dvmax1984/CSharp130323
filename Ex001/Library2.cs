b05c458a9d693b5a4a01b49a9044227e288522ed
Maksim <dvmax1984@mail.ru>
2023-03-13 14:41:17 +0700

    Главный метод

--- Ex001/Library2.cs
+++ Ex001/Library2.cs
@@ -1,6 +1,6 @@
 public static class Library2
 {
-    public static string PrintArray(int)string printArray(int[] array)
+    public static string printArray(int[] array)
     {
         string nArray = String.Empty;
         for (int i = 0; i < array.Length; i++)
--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Library1;
+using static Library2;
+
+int[] array = {1, 2, 5, 6, 7, 3};
+
+
+//ArrayTurn
