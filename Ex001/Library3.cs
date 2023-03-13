dd429f3fd318bd909e99d5c348c9b176fe042782
Artem <shenlin@yandex.ru>
2023-03-13 10:38:58 +0300

    создание массива

---
+++ Ex001/Library3.cs
@@ -0,0 +1,11 @@
+public static class Library
+{
+    public static int[] makeArray()
+    {
+        int[] array = new int[5];
+        Random rand = new Random();
+        for (int i = 0; i< array.Length; i++)
+        array[i] = rand.Next();
+        return array;
+    }
+}
